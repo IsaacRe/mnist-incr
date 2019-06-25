5dc4083ed4669d3f89f4738afd126bafafe783f5
Isaac Rehg <isaacrehg@gmail.com>
2019-06-25 12:29:27 -0400

    setup incremental learning

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torch.utils.data import SubsetRandomSampler, Subset, DataLoader, Dataset, ConcatDataset
+import numpy as np
 
 
 class Net(nn.Module):
@@ -36,15 +38,18 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('Train Iteration: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
 
 
+accs = []
 def test(args, model, device, test_loader):
+    global accs
     model.eval()
     test_loss = 0
     correct = 0
+    totals = [np.where(test_loader.dataset.train_labels == i)[0].shape[0] for i in range(10)]
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
@@ -59,6 +64,11 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+    accs = np.concatenate([accs, np.array([100. * correct / len(test_loader.dataset)])])
+
+    np.savez('accs_%d-lexp-len_%d-explr.npz' % (args.lexp_len, args.num_explr),
+             accs=accs)
+
 
 def main():
     # Training settings
@@ -67,7 +77,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--num_iters', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
@@ -80,8 +90,12 @@ def main():
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
 
-    parser.add_argument('--save-model', action='store_true', default=False,
+    parser.add_argument('--no-save', action='store_false', dest='save_model',
                         help='For Saving the current Model')
+
+    # incremental training args
+    parser.add_argument('--num-exemplars', type=int, dest='num_explr')
+    parser.add_argument('--lexp-len', type=int, default=100) # Full dataset has > 5000 samples per class
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -89,15 +103,15 @@ def main():
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
+    exemplars = {}
+
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
+    train_set = datasets.MNIST('../data', train=True, download=True,
+                             transform=transforms.Compose([
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.1307,), (0.3081,))
+                             ]))
+    test_loader = DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
@@ -107,12 +121,30 @@ def main():
     model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+    itr = 0
+    for epoch in range(0, args.num_iters, 10):
+        perm_id = np.random.choice(10, 10, replace=False)
+        for i in perm_id:
+            # select data for learning exposure
+            indices = np.random.choice(np.where(train_set.train_labels == i)[0], args.lexp_len, replace=False)
+            # add exemplars
+            explr_indices = np.array([])
+            for c in exemplars:
+                if c == i:
+                    continue
+                explr_indices = np.concatenate([explr_indices, exemplars[c],
+                                                np.random.choice(exemplars[c], args.lexp_len - args.num_explr)])
+            sampler = torch.utils.data.SubsetRandomSampler(np.concatenate([indices, explr_indices]).astype('int64'))
+            train_loader = torch.utils.data.DataLoader(train_set,
+                                                       batch_size=args.batch_size, shuffle=False, sampler=sampler,
+                                                       **kwargs)
+            train(args, model, device, train_loader, optimizer, itr)
+            test(args, model, device, test_loader)
+            exemplars[i] = np.random.choice(indices, args.num_explr, replace=False)
+            itr += 1
 
     if (args.save_model):
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        torch.save(model.state_dict(), "mnist%d-lexp-len_%d-explr.pt" % (args.lexp_len, args.num_explr))
 
 
 if __name__ == '__main__':
