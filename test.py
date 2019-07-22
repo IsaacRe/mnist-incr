ed6f25dd15f5dfb87096b268d231eea62a6bee79
Isaac Rehg <isaacrehg@gmail.com>
2019-07-22 16:42:07 -0700

    add option for test/train dset selection in get_batch_suite

--- test.py
+++ test.py
@@ -5,15 +5,16 @@ import torch
 from main import Net
 
 
-def get_batch_suite(batch_size, dset_size=10):
+def get_batch_suite(batch_size, dset_size=None, train=True):
     net = Net()
     x_ent = nn.CrossEntropyLoss()
-    d_set = datasets.MNIST('../data', train=True, download=True,
+    d_set = datasets.MNIST('../data', train=train, download=True,
                                transform=transforms.Compose([
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.1307,), (0.3081,))
                                ]))
-    d_set = torch.utils.data.Subset(d_set, np.arange(dset_size))
+    if dset_size is not None:
+        d_set = torch.utils.data.Subset(d_set, np.arange(dset_size))
     loader = torch.utils.data.DataLoader(d_set, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
 
     return net, x_ent, loader
