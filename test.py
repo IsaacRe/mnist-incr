4744f8685de3ab49a62cbd17cb575fd49de64e9a
Isaac Rehg <isaacrehg@gmail.com>
2019-07-23 14:46:09 -0700

    add incremental test suite function

--- test.py
+++ test.py
@@ -18,3 +18,22 @@ def get_batch_suite(batch_size, dset_size=None, train=True):
     loader = torch.utils.data.DataLoader(d_set, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
 
     return net, x_ent, loader
+
+
+def get_incr_suite(batch_size, dset_size=None, train=True):
+    net = Net()
+    x_ent = nn.CrossEntropyLoss()
+    d_set = datasets.MNIST('../data', train=train, download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))
+                           ]))
+
+    def make_sampler(label):
+        select = np.where(d_set.train_labels == label)[0]
+        indices = np.random.choice(select, dset_size if dset_size is not None else len(select), replace=False)
+        return torch.utils.data.SubsetRandomSampler(indices)
+    loaders = [torch.utils.data.DataLoader(d_set, batch_size=batch_size, shuffle=False, sampler=make_sampler(l),
+                                           num_workers=1, pin_memory=True) for l in range(10)]
+
+    return net, x_ent, loaders
