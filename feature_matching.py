6e5af061b3321914d28a7e31437c7491f8562015
Isaac Rehg <isaacrehg@gmail.com>
2019-08-29 19:07:31 -0400

    add implementation for correlation matrix computation, outline feature matching

--- feature_matching.py
+++ feature_matching.py
@@ -26,6 +26,10 @@ class CorrelationTracker:
         """
         self.net = net
         self.feature_idx = feature_idx
+        self.n_feat = None
+
+        if net is not None:
+            self.n_feat = net.features[feature_idx].out_features
 
         # TODO implement on-line correlation computation
         self.store_on_gpu = store_on_gpu
@@ -71,14 +75,14 @@ class CorrelationTracker:
         """
         Compute the within-net correlation on the provided data.
         :param dataloader: the dataloader
-        :param net: the trained CNN to use
+        :param net: the trained CNN
         :param feature_idx: index of the layer to use. If None, will use self.feature_idx
         :return: the correlation matrix
         """
         feature_idx = self.feature_idx if feature_idx is None else feature_idx
         assert feature_idx is not None, "Attribute, 'feature_idx' is None and no feature_idx was explicitly provided."
 
-        # get mean activation over the data of each feature
+        # get mean activation over the data for each feature
         [mu] = self._get_feature_means(dataloader, feature_idx, net)
 
         corr_matr = None
@@ -86,38 +90,103 @@ class CorrelationTracker:
         total = None
         for i, (idx, images, labels) in enumerate(dataloader):
             total += len(labels)
+            out = images
             for j, layer in enumerate(net.features):
-                out = layer(images)
+                out = layer(out)
                 if feature_idx == j:
                     break
-            out = torch.mean(out, dim=(0, 1, 2))  # average across batch and spatial dimensions
-            if sig is None:
-                sig = (out - mu)**2
-            else:
-                sig += (out - mu)**2
-            # TODO Compute A where A_ij = (out_i - mu_i) * (out_j - mu_j)
-            corr_matr += A
-        sig /= total
-        corr_matr /= total
+            # TODO make sure dimension 3 is what I think it is
+            out = out.flatten(dim=(0, 1, 2))  # [(batch * width * height) X filters]
+            assert self.n_feat == out.shape[1], "Output of layer %d number of features does not match self.n_feat"
+            n_feat = self.n_feat
 
-        # TODO compute S where S_ij = sig_i * sig_j
-        # TODO compute corr_matr /= S (elem-wise division)
+            # get x_i - mu_i, the deviation of each feature output from its mean
+            deviation = out - mu  # [(B * W * H) X F]
+            # corr_matr_temp_ij = deviation_i * deviation_j
+            deviation_expanded = deviation.unsqueeze(2).repeat(1, 1, n_feat)  # [(B * W * H) X F X F]
+            corr_matr_temp = deviation_expanded * deviation_expanded.view(0, 2, 1)  # [(B * W * H) X F X F]
+            # sum over batch and spatial dimensions
+            corr_matr_temp = torch.sum(corr_matr_temp, dim=0)  # [F X F]
+            if corr_matr is None:
+                corr_matr = corr_matr_temp
+            else:
+                corr_matr += corr_matr_temp
+
+            # accumulate feature-wise variances for this match
+            if sig is None:
+                sig = torch.mean(deviation**2, dim=0)  # [F]
+            else:
+                sig += torch.mean(deviation**2, dim=0)  # [F]
+
+        sig = (sig / total)**0.5  # feature-wise std deviation
+        corr_matr /= total  # un-normalized correlation matrix
+
+        # sig_matr_ij = sig_i * sig_j
+        sig_expanded = sig.unsqueeze(1).repeat(1, n_feat)  # [F X F]
+        sig_matr = sig_expanded * sig_expanded.view(1, 0)
+        corr_matr /= sig_matr  # normalized correlation matrix
 
         return corr_matr
 
-    def between_net_corr(self, dataloader, net_1=None, net_2=None, feature_idx=None):
+    def between_net_corr(self, dataloader, net_1, net_2, feature_idx=None):
         """
-        Return the correlation matrix of features. If
-        :param net_1:
-        :param dataloader:
-        :param net_2:
+        Compute the between-net correlation for net_1, net_2 on the provided data
+        :param dataloader: the dataloader
+        :param net_1: the first trained CNN
+        :param net_2: the second trained CNN
         :return: the correlation matrix
         """
         feature_idx = self.feature_idx if feature_idx is None else feature_idx
         assert feature_idx is not None, "Attribute, 'feature_idx' is None and no feature_idx was explicitly provided."
 
-        # TODO
-        pass
+        # get mean activations over the data for each feature for nets 1 and 2
+        mu = self._get_feature_means(dataloader, feature_idx, net_1, net_2)
+
+        corr_matr = None
+        sigs = [None, None]
+        total = None
+        for i, (idx, images, labels) in enumerate(dataloader):
+            total += len(labels)
+            deviations = [None, None]
+            for j, net in enumerate([net_1, net_2]):
+                out = images
+                for k, layer in enumerate(net.features):
+                    out = layer(out)
+                    if feature_idx == k:
+                        break
+
+                out = out.flatten(dim=(0, 1, 2))  # [(batch * width * height) X filters]
+                assert self.n_feat == out.shape[1], "Output of layer %d number of features does not match self.n_feat"
+                n_feat = self.n_feat
+
+                # get x_i - mu_i, the deviation of each feature output from its mean
+                deviations[j] = out - mu[j]  # [(B * W * H) X F]
+
+            # corr_matr_temp_ij = deviations[0]_i * deviations[1]_j
+            dev_1_expanded, dev_2_expanded = [dev.unsqueeze(2).repeat(1, 1, n_feat) for dev in deviations]  # [(B * W * H) X F X F] (each)
+            corr_matr_temp = dev_1_expanded * dev_2_expanded.view(0, 2, 1)  # [(B * W * H) X F X F]
+            # sum over batch and spatial dimensions
+            corr_matr_temp = torch.sum(corr_matr_temp, dim=0)  # [F X F]
+            if corr_matr is None:
+                corr_matr = corr_matr_temp
+            else:
+                corr_matr += corr_matr_temp
+
+            # accumulate feature-wise variances for this match
+            if sigs == [None, None]:
+                sigs = [torch.mean(dev**2, dim=0) for dev in deviations] # [F] (each)
+            else:
+                sigs += [torch.mean(dev**2, dim=0) for dev in deviations] # [F] (each)
+
+        sig = [(sig / total)**0.5 for sig in sigs]  # feature-wise std deviation
+        corr_matr /= total  # un-normalized correlation matrix
+
+        # sig_matr_ij = sigs[0]_i * sigs[1]_j
+        # TODO potential bug point
+        sig_matr = sigs[0].unsqueeze(1).repeat(1, n_feat) * sigs[1].unsqueeze(0).repeat(n_feat, 1)
+        corr_matr /= sig_matr  # normalized correlation matrix
+
+        return corr_matr
 
 
 class FeatureMatcher:
